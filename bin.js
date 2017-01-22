317110304538ab4f5855bb5fdc020d507752185e
ohataken <ohataken@outlook.com>
2017-01-23 05:52:46 +0900

    Add bin.js

---
+++ bin.js
@@ -0,0 +1,45 @@
+const rpc = require("node-json-rpc");
+const karenWilson = require("./index");
+
+HOST = process.env.HOST || "192.168.1.1";
+USERNAME = process.env.USERNAME || "admin";
+PASSWORD = process.env.PASSWORD || "password";
+
+const client = new rpc.Client({
+  port: 80,
+  host: HOST,
+  path: "/api",
+  strict: true,
+});
+
+const subcommandTable = {
+  clients: {
+    description: "dhcp clients",
+    proc: () => {
+      karenWilson.login(client, 1, {
+        id: USERNAME,
+        password: PASSWORD,
+      }, (err, res) => {
+        karenWilson.basicGetStatus(client, 1, res.token, (err, res) => {
+          res.result.dhcp_clients.forEach((cl) => {
+            console.log(cl);
+          });
+        });
+      });
+    }
+  },
+
+  help: {
+    description: "help",
+    proc: () => {
+      const keys = Object.keys(subcommandTable);
+      keys.forEach((key) => {
+        console.log(subcommandTable[key].description);
+      });
+    }
+  }
+};
+
+if (process.argv.length > 2 && process.argv[2] in subcommandTable) {
+  subcommandTable[process.argv[2]].proc();
+}
